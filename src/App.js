d732fc14f688b7c0c9c37891aa499976c17460b2
kavithashanmugan <kavitha.shanmugan@gmail.com>
2021-05-30 22:34:07 -0600

    experimenting koi template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code> src / App.js </code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         <Header/>
         </a>
         <a
           className="App-link"
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+      <div>Rainbow Header</div>
+  );
+};
+
+export default Header;
